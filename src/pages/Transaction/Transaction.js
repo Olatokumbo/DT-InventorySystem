e1dc385a4316ae1722c254c237aeadf73a81ea0e
David Odesola <faithodesola@gmail.com>
2020-11-18 23:15:54 +0800

    Structuring the Transaction Page

--- src/pages/Transaction/Transaction.js
+++ src/pages/Transaction/Transaction.js
@@ -1,20 +1,40 @@
 import React from "react";
-import { TextField } from "@material-ui/core";
+import { TextField, Typography } from "@material-ui/core";
 import style from "./Transaction.module.css";
-const Transaction = () =>{
-    return(
-        <div className={style.transaction}>
-        <TextField 
-        size="large" 
-        variant="outlined"
-        label="Search Asset Number"
-        className={style.input}
+const Transaction = () => {
+  return (
+    <div className={style.transaction}>
+      <div className={style.top}>
+      <div className={style.container}>
+        <TextField
+          size="large"
+          variant="outlined"
+          label="Search Asset Number"
+          className={style.input}
         />
-        <div className={style.data}>
-        
+      </div>
+      <div className={style.container}>
+        <TextField
+          size="large"
+          variant="outlined"
+          label="Employee Id"
+          className={style.input}
+        />
+      </div>
+      </div>
+      <div className={style.data}>
+        <div className={style.statusContainer}>
+          <Typography>Status:</Typography>
+          <Typography className={style.approved}>APPROVED</Typography>
         </div>
+        <Typography className={style.currentUser}>David King</Typography>
+        <div className={style.dates}>
+        <Typography>Start Date: Tuesday, 10/12/2020</Typography>
+        <Typography>End Date: Tuesday, 10/12/2020</Typography>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Transaction;
