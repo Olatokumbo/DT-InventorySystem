f66839809f132acc3b095a0135db6d5f572de313
David Odesola <faithodesola@gmail.com>
2020-11-24 00:45:26 +0800

    Finishing the Asset's Info Page

--- src/pages/AssetInfo/AssetInfo.js
+++ src/pages/AssetInfo/AssetInfo.js
@@ -5,9 +5,12 @@ import {
   Button,
   FormControlLabel,
   Switch,
+  IconButton,
 } from "@material-ui/core";
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import axios from "axios";
 import moment from "moment";
+import { useHistory } from "react-router-dom";
 import style from "./AssetInfo.module.css";
 
 const AssetInfo = ({
@@ -15,18 +18,92 @@ const AssetInfo = ({
     params: { assetId },
   },
 }) => {
-  const [assetData, setAssetData] = useState({});
+  const history = useHistory();
   const [editState, setEditState] = useState(true);
+  const [uid, setUid] = useState("");
+  const [machineType, setMachineType] = useState("");
+  const [makeAndModel, setMakeAndModel] = useState("");
+  const [serviceTag, setServiceTag] = useState("");
+  const [machineNumber, setMachineNumber] = useState("");
+  const [deliveryDate, setDeliveryDate] = useState("");
+  const [resourceAccount, setResourceAccount] = useState("");
+  const [currentUser, setCurrentUser] = useState("");
+  const [deploymentDate, setDeploymentDate] = useState("");
+  const [businessUnit, setBusinessUnit] = useState("");
+  const [location, setLocation] = useState("");
+  const [poNumber, setPoNumber] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [moveable, setMoveable] = useState(false);
+
   useEffect(() => {
     axios.get(`http://localhost:7000/asset/${assetId}`).then((received) => {
       console.log(received.data[0]);
-      setAssetData(received.data[0]);
+      setUid(received.data[0].uid);
+      setMachineType(received.data[0].machineType);
+      setMachineNumber(received.data[0].machineNumber);
+      setMakeAndModel(received.data[0].makeAndModel);
+      setServiceTag(received.data[0].serviceTag);
+      setDeliveryDate(received.data[0].deliveryDate);
+      setDeploymentDate(received.data[0].deploymentDate);
+      setResourceAccount(received.data[0].resourceAccount);
+      setCurrentUser(received.data[0].currentUser);
+      setBusinessUnit(received.data[0].businessUnit);
+      setLocation(received.data[0].location);
+      setPoNumber(received.data[0].poNumber);
+      setStartDate(received.data[0].startDate);
+      setEndDate(received.data[0].endDate);
+      setMoveable(received.data[0].moveable === 1 ? true : false);
     });
   }, [assetId]);
+
+  const deleteAsset = async (uid) => {
+    axios
+      .post(`http://localhost:7000/asset/delete/${uid}`)
+      .then(() => {
+        // console.log(result);
+        alert("Asset has been deleted from the Inventory");
+        history.push("/home");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const editAsset = async (uid) => {
+    axios
+      .post(`http://localhost:7000/asset/edit/${uid}`, {
+        machineType,
+        machineNumber,
+        makeAndModel,
+        serviceTag,
+        deliveryDate,
+        deploymentDate,
+        resourceAccount,
+        currentUser,
+        businessUnit,
+        location,
+        poNumber,
+        moveable,
+      })
+      .then(() => {
+        //   console.log(result);
+        alert("Asset Info has been Updated");
+        setEditState(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <div className={style.assetInfo}>
       <div className={style.header}>
-        <h4>Asset Info</h4>
+        <div className={style.main}>
+          <IconButton size="small" onClick={()=>history.push("/home")}>
+            <ArrowBackIcon />
+          </IconButton>
+          <h4>Asset Info</h4>
+        </div>
         <div className={style.actions}>
           <Button
             variant="contained"
@@ -37,10 +114,10 @@ const AssetInfo = ({
             {editState ? "Edit" : "Lock"}
           </Button>
           <Button
-            onClick={() => {}}
             variant="contained"
             size="small"
             color="secondary"
+            onClick={() => deleteAsset(uid)}
           >
             Delete
           </Button>
@@ -51,7 +128,8 @@ const AssetInfo = ({
           label="Machine Type"
           disabled={editState}
           className={style.input}
-          value={assetData.machineType}
+          value={machineType}
+          onChange={(e) => setMachineType(e.target.value)}
           InputLabelProps={{
             shrink: true,
           }}
@@ -60,7 +138,8 @@ const AssetInfo = ({
           label="Machine Number"
           disabled={editState}
           className={style.input}
-          value={assetData.machineNumber}
+          value={machineNumber}
+          onChange={(e) => setMachineNumber(e.target.value)}
           InputLabelProps={{
             shrink: true,
           }}
@@ -69,7 +148,8 @@ const AssetInfo = ({
           label="Make and Model"
           disabled={editState}
           className={style.input}
-          value={assetData.makeAndModel}
+          value={makeAndModel}
+          onChange={(e) => setMakeAndModel(e.target.value)}
           InputLabelProps={{
             shrink: true,
           }}
@@ -78,7 +158,8 @@ const AssetInfo = ({
           label="Business Unit"
           disabled={editState}
           className={style.input}
-          value={assetData.businessUnit}
+          value={businessUnit}
+          onChange={(e) => setBusinessUnit(e.target.value)}
           InputLabelProps={{
             shrink: true,
           }}
@@ -87,7 +168,8 @@ const AssetInfo = ({
           label="Location"
           disabled={editState}
           className={style.input}
-          value={assetData.location}
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
           InputLabelProps={{
             shrink: true,
           }}
@@ -96,7 +178,8 @@ const AssetInfo = ({
           label="PO Number"
           disabled={editState}
           className={style.input}
-          value={assetData.poNumber}
+          value={poNumber}
+          onChange={(e) => setPoNumber(e.target.value)}
           InputLabelProps={{
             shrink: true,
           }}
@@ -105,7 +188,8 @@ const AssetInfo = ({
           label="Service Tag"
           disabled={editState}
           className={style.input}
-          value={assetData.serviceTag}
+          value={serviceTag}
+          onChange={(e) => setServiceTag(e.target.value)}
           InputLabelProps={{
             shrink: true,
           }}
@@ -116,7 +200,8 @@ const AssetInfo = ({
             disabled={editState}
             type="date"
             className={style.input}
-            value={assetData.deliveryDate}
+            value={deliveryDate}
+            onChange={(e) => setDeliveryDate(e.target.value)}
             InputLabelProps={{
               shrink: true,
             }}
@@ -126,7 +211,8 @@ const AssetInfo = ({
             disabled={editState}
             type="date"
             className={style.input}
-            value={assetData.deploymentDate}
+            value={deploymentDate}
+            onChange={(e) => setDeploymentDate(e.target.value)}
             InputLabelProps={{
               shrink: true,
             }}
@@ -136,7 +222,8 @@ const AssetInfo = ({
           label="Current User"
           disabled={editState}
           className={style.input}
-          value={assetData.currentUser}
+          value={currentUser}
+          onChange={(e) => setCurrentUser(e.target.value)}
           InputLabelProps={{
             shrink: true,
           }}
@@ -145,7 +232,8 @@ const AssetInfo = ({
           label="Resource Account"
           disabled={editState}
           className={style.input}
-          value={assetData.resourceAccount}
+          value={resourceAccount}
+          onChange={(e) => setResourceAccount(e.target.value)}
           InputLabelProps={{
             shrink: true,
           }}
@@ -157,7 +245,8 @@ const AssetInfo = ({
               name="Moveable"
               color="primary"
               disabled={editState}
-              checked={assetData.moveable === 1 ? true : false}
+              onChange={(e) => setMoveable(e.target.checked)}
+              checked={moveable}
             />
           }
           label="Moveable"
@@ -169,11 +258,7 @@ const AssetInfo = ({
             type="text"
             className={style.input}
             disabled
-            value={
-              assetData.endDate
-                ? moment(assetData.startDate).format("DD/MM/YYYY")
-                : "No date"
-            }
+            value={endDate ? moment(startDate).format("DD/MM/YYYY") : "No date"}
             InputLabelProps={{
               shrink: true,
             }}
@@ -183,11 +268,7 @@ const AssetInfo = ({
             type="text"
             className={style.input}
             disabled
-            value={
-              assetData.endDate
-                ? moment(assetData.endDate).format("DD/MM/YYYY")
-                : "No date"
-            }
+            value={endDate ? moment(endDate).format("DD/MM/YYYY") : "No date"}
             InputLabelProps={{
               shrink: true,
             }}
@@ -196,6 +277,7 @@ const AssetInfo = ({
         <Button
           variant="contained"
           disabled={editState}
+          onClick={() => editAsset(uid)}
           color="primary"
           size="medium"
         >
